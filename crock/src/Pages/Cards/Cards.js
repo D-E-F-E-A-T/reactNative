a86bf219fa9be0056c924ac181e6036f7b455062
marcelorafael <marcelorafael23@hotmail.com>
2020-06-30 08:10:39 -0300

    Primeiro comit do crock

---
+++ crock/src/Pages/Cards/Cards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { 
+    Container, Input, TextButton, Button, Form,
+    Title, CantainerForm, TextFooter, TextSignUp,
+    ContainerTextButton,
+ } from './styles';
+
+export default function Cards() {
+ return (
+   <Container>
+       
+   </Container>
+  );
+}
---
+++ crock/src/Pages/SignIn/SignIn.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StatusBar, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { 
+    Container, Input, TextButton, Button, Form,
+    Title, CantainerForm, TextFooter, TextSignUp,
+    ContainerTextButton,
+ } from './styles';
+
+export default function SignIn({ navigation }) {
+ return (
+   <Container>
+       <CantainerForm>
+        <Form>
+            <Title> Login </Title>
+            <Input
+                placeholder="Usuário"
+            />
+
+            <Input
+                placeholder="Senha"
+            />
+
+            <Button onPress={()=> navigation.navigate("Tabs")}>
+                <ContainerTextButton>
+                    <TextButton> Login </TextButton>
+                    <Icon name="ios-arrow-round-forward" color="#FFF" size={20} />
+                </ContainerTextButton>
+            </Button>
+
+            
+
+            
+        </Form>
+
+        
+       </CantainerForm>
+        
+       <Button onPress={()=> navigation.navigate("SignUp") }>
+            <TextFooter> Ainda não possui conta?  <TextSignUp> Criar Conta </TextSignUp> </TextFooter>
+        </Button>
+   </Container>
+  );
+}
